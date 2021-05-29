3ba42cd7e75958588528b9de23ea60c38049aa77
Avinash Agarwal <agarwalavinash14@gmail.com>
2021-05-30 02:32:47 +0530

    client added

--- client/src/App.js
+++ client/src/App.js
@@ -10,37 +10,11 @@ class App extends Component {
 
   getVideosList = async () =>  {
     const videos = await axios.get('/api/videos');
-    this.setState({videoListFromAPI: videos.data.data, currentVideoList: videos.data.data});
+    this.setState({videoListFromAPI: videos.data.data.slice(), currentVideoList: videos.data.data.slice()});
   }
 
   componentDidMount() {
     this.getVideosList();
-  }
-
-  render () {
-    return (
-      <div className="jumbotron">
-        <nav className="navbar navbar-dark bg-dark">
-          <a className="navbar-brand" href="#">Youtube Query</a>
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>    
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-              </ul>
-        </nav>
-        {this.state.currentVideoList.length > 0?
-        this.generateVideoList()
-        :null}
-      </div>
-    )
   }
 
   generateVideoList =()=>{
@@ -48,7 +22,7 @@ class App extends Component {
       return (
         <div class="col-auto mb-3">
           <div class="card" style={{"width": "16rem"}}>
-            <img class="card-img-top" src={video.thumbnails.default.url} alt="Card image cap"/>
+            <img class="card-img-top" src={video.thumbnails.medium.url} alt="Card image cap"/>
             <div class="card-body">
               <p class="card-title">{video.title}</p>
             </div>
@@ -65,6 +39,67 @@ class App extends Component {
       );
   }
 
+  render () {
+    return (
+      <div className="jumbotron">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Youtube Query API</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Filters (Sort By)
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><button class="dropdown-item" onClick={this.sortByTitle}>Title</button></li>
+                    <li><button class="dropdown-item" onClick={this.sortByPublishedDate}>Published Date</button></li>
+                    <li><button class="dropdown-item" onClick={this.reset}>Reset</button></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="form-inline ms-auto">
+                <input id="input" class="form-control mr-sm-2" style={{"display":"inline","width":"75%","margin-right":"10px"}} type="search" placeholder="Search" aria-label="Search"/>
+                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit" onClick={this.searchQuery}>Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+        {this.state.currentVideoList.length > 0?
+        this.generateVideoList()
+        :null}
+      </div>
+    )
+  }
+
+  sortByTitle = () => {
+    let {currentVideoList} = this.state;
+    currentVideoList.sort((a, b) => a.title.localeCompare(b.title));
+    this.setState({currentVideoList});
+  } 
+
+  sortByPublishedDate = () => {
+    let {currentVideoList} = this.state;
+    currentVideoList.sort(function(a,b){
+      return new Date(b.publishTime) - new Date(a.publishTime);
+    });
+    this.setState({currentVideoList});
+  } 
+
+  reset = () => {
+    this.setState({currentVideoList: this.state.videoListFromAPI.slice()});
+  } 
+
+  searchQuery = async (event)=> {
+    event.preventDefault();
+    const query = document.getElementById("input").value;
+    document.getElementById("input").value = '';
+    const queryVideoList = await axios.get(`/api/videos/search?q=${query}&page=1&limit=200`); 
+    this.setState({currentVideoList: queryVideoList.data.data.slice()});
+  }
 }
 
 export default App;
---
+++ util/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://localhost:27017/youtube-query',
+    apiKey: 'AIzaSyBZkSN2aZ7V-q8eVnaQyxgJJlnKSCfUI14'
+}
